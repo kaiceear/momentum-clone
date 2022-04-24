12a93546c4a112360ca118c628b9b6164c6274c8
kaiceear <kaiceear@gmail.com>
2022-04-25 01:12:13 +0900

    update all files

---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+const quotes = [
+  {
+    quote: "삶이 있는 한 희망은 있다.",
+    author: "키케로",
+  },
+  {
+    quote: "산다는것 그것은 치열한 전투이다.",
+    author: "로망로랑",
+  },
+  {
+    quote: "언제나 현재에 집중할수 있다면 행복할것이다.",
+    author: "파울로 코엘료",
+  },
+  {
+    quote: "진정으로 웃으려면 고통을 참아야하며 , 나아가 고통을 즐길 줄 알아야 한다.",
+    author: "찰리 채플린",
+  },
+  {
+    quote: "먼저 자신을 비웃어라. 다른 사람이 당신을 비웃기 전에",
+    author: "엘사 멕스월",
+  },
+  {
+    quote: "오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다.",
+    author: "앙드레 말로",
+  },
+  {
+    quote: "성공의 비결은 단 한 가지, 잘할 수 있는 일에 광적으로 집중하는 것이다.",
+    author: "톰 모나건",
+  },
+  {
+    quote: "자신감 있는 표정을 지으면 자신감이 생긴다.",
+    author: "찰스 다윈",
+  },
+]
+
+const quote = document.querySelector("#quote div:first-child");
+const author = document.querySelector("#quote div:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
---
+++ js/todo.js
@@ -0,0 +1,59 @@
+const toDoForm = document.getElementById("todo-form");
+
+//const toDoInput = toDoForm.querySelector("input");
+const toDoInput = document.querySelector("#todo-form input"); //위의 코드와 정확하게 동일한 기능
+
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo)
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+  }
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos; //localStorage에서 받아온 배열을 toDos에 넣어 복원한다
+  parsedToDos.forEach(paintToDo);
+}
+
